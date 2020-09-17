57e9480dc5f889bdfd40e540708a31c07d0efd94
Harry LB <harry.lingardbright@gmail.com>
2020-09-17 09:47:55 +0100

    added bandit animation

--- src/Components/canvas/canvas.js
+++ src/Components/canvas/canvas.js
@@ -2,13 +2,16 @@ import React, { useRef, useEffect, useCallback } from 'react';
 import healthbar from '../../Components/healthbar/healthbar'
 import enemyHealthbar from '../../Components/healthbar/enemyHealthbar'
 import Wizard from '../characterAnimation/wizard.js'
+import Bandit from '../characterAnimation/bandit.js'
 
 const Canvas = (props) => {
 
     const canvasRef = useRef(null)
 
     useEffect(() => {
+      Bandit();
       Wizard(); 
+     
       let animationFrameId
      
       return () => {
