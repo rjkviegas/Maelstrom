3d44f1fd3b63a1852bd956d0be349ccd0d99fbc4
Daniel M <danielim@hotmail.co.uk>
2020-09-17 17:02:28 +0100

    routing

--- src/Components/fight/fight.js
+++ src/Components/fight/fight.js
@@ -1,7 +1,9 @@
-import React, { useContext, useState, useReducer } from 'react'
-import playerReducer from '../../Reducers/playerReducer.js'
+import React, { useContext } from 'react'
 import PlayerContext from '../../config/playerContext.js'
 import OpponentContext from '../../config/opponentContext.js'
+import { Player } from '../../Components/player/player.js'
+import { Opponent } from '../../Components/opponent/opponent.js'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function Fight() {
     const { PlayerObj, dispatch }  = useContext(PlayerContext)
@@ -11,6 +13,8 @@ export default function Fight() {
         <div style={{visibility: (PlayerObj.hp > 0 && OpponentObj.hp <= 0) ? "hidden" : "visible"}} ><button onClick={() => 
           dispatchOpp({type: 'attack', payload: 10})} 
           >Attack</button>
+            <Player/>
+            <Opponent/>
         </div>
         <div data-testid="attack_button" style={{visibility: (OpponentObj.hp <= 0) ? "visible" : "hidden" }}>
           <h1 data-testid="h1">YOU WIN</h1>
