4e2cdeacaed8ab0987e7dd072241ecd861693166
Harry LB <harry.lingardbright@gmail.com>
2020-09-22 17:54:05 +0100

    some testing shiz i cant even remember anymore

--- src/Reducers/reducers.test.js
+++ src/Reducers/reducers.test.js
@@ -7,4 +7,11 @@ describe('player reducer', () => {
     expect(playerReducer(undefined, mockCallBack)).toBeTruthy();
     expect(playerReducer(undefined, {})).toEqual({});
   });
+  // it('attacked reduces health', () => {
+  //   const [PlayerObj, dispatch] = useReducer(playerReducer, new player())
+  //   dispatch({type: 'attacked', payload: 10})
+  //   playerReducer(PlayerObj, { type: "attacked", payload: 10 })
+ 
+  //   expect((PlayerObj.hp)).toEqual(90);
+  // });
 });
