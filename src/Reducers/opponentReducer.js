682a23120c4cb6e9371afebc9c27598c67bba48b
Daniel M <danielim@hotmail.co.uk>
2020-09-23 17:27:57 +0100

    Reducer using action.payload rather than hardcoded stuff

--- src/Reducers/opponentReducer.js
+++ src/Reducers/opponentReducer.js
@@ -1,18 +1,15 @@
-import King from "../Components/classes/king/king.js";
 import generateRandomOpponent from "../Components/classes/opponentGenerator.js";
 
 export default function opponentReducer(state, action) {
     switch(action.type) {
       case "attacked":       
         return { ...state, hp: state.hp - action.payload}
-      // case "rename":
-      //   return {...state, name: action.payload}
       case "set_attack": 
         return { ...state, is_attacking: action.payload}
       case "unset_attack": 
         return { ...state, is_attacking: action.payload}   
       case "reset":
-        return new generateRandomOpponent();
+        return action.payload;
       default: 
         return {...state};
     }
