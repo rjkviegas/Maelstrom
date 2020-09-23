b081fe16a1d95334ccff245f684dce08c3ac07fb
Daniel M <danielim@hotmail.co.uk>
2020-09-23 09:20:16 +0100

    King using super

--- src/Components/classes/king/king.js
+++ src/Components/classes/king/king.js
@@ -1,24 +1,18 @@
 import { kingAttack } from "../../characterAnimation/king/king_attack";
 import { kingDeath } from "../../characterAnimation/king/king_dead";
 import { kingIdle } from "../../characterAnimation/king/king_idle";
+import Character from "../character_super/character_super";
 
 const STARTING_HITPOINTS = 185;
 
-class King {
+class King extends Character{
   constructor(idleImage = kingIdle, attackImage = kingAttack, deathImage = kingDeath, deathFrameNumber = 7, idleSourceY = 0, deathSourceY = 0, attackSourceY = 0){
+    super(idleImage, attackImage, deathImage, deathFrameNumber, idleSourceY, deathSourceY, attackSourceY)
     this.name = 'Opponent_Placeholder'
     this.hp = STARTING_HITPOINTS
     this.MAX_HP = STARTING_HITPOINTS
     this.is_attacking = false;
     this.money = 0
-    this.idleSourceY = idleSourceY
-    this.deathSourceY = deathSourceY
-    this.attackSourceY = attackSourceY
-    this.idleImage = idleImage
-    this.attackImage = attackImage
-    this.deathImage = deathImage
-    this.deathFrameNumber = deathFrameNumber
-    this.isDead = () => { return this.hp <= 0}
   }
 }
 
