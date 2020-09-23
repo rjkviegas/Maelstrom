f981d2e44bd7bec950bf33da22973138b83f8b04
Harry LB <harry.lingardbright@gmail.com>
2020-09-23 14:11:56 +0100

    commented out unused method

--- src/Reducers/opponentReducer.js
+++ src/Reducers/opponentReducer.js
@@ -4,8 +4,8 @@ export default function opponentReducer(state, action) {
     switch(action.type) {
       case "attacked":       
         return { ...state, hp: state.hp - action.payload}
-      case "rename":
-        return {...state, name: action.payload}
+      // case "rename":
+      //   return {...state, name: action.payload}
       case "set_attack": 
         return { ...state, is_attacking: action.payload}
       case "unset_attack": 
