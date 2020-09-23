9a8ad8d44438e0e0fe25868327ee598a9a523cfa
Daniel M <danielim@hotmail.co.uk>
2020-09-23 09:19:28 +0100

    Slime using super

--- src/Components/classes/slime/slime.js
+++ src/Components/classes/slime/slime.js
@@ -1,25 +1,19 @@
 import { slimeAttack } from "../../characterAnimation/slime/slime_attack";
 import { slimeDeath } from "../../characterAnimation/slime/slime_death";
 import { slimeIdle } from "../../characterAnimation/slime/slime_idle";
+import Character from "../character_super/character_super";
 
 const STARTING_HITPOINTS = 50;
 
-export default class Slime {
+export default class Slime extends Character {
 
     constructor(idleImage = slimeIdle, attackImage = slimeAttack, deathImage = slimeDeath, deathFrameNumber = 6, idleSourceY = 0, deathSourceY = 2, attackSourceY = 1){
-        this.name = 'Opponent_Placeholder'
+        super(idleImage, attackImage, deathImage, deathFrameNumber, idleSourceY, deathSourceY, attackSourceY)
+        this.name = 'Slime'
         this.hp = STARTING_HITPOINTS
         this.MAX_HP = STARTING_HITPOINTS
-        this.is_attacking = false;
         this.money = 0
-        this.idleSourceY = idleSourceY
-        this.deathSourceY = deathSourceY
-        this.attackSourceY = attackSourceY
-        this.idleImage = idleImage
-        this.attackImage = attackImage
-        this.deathImage = deathImage
-        this.deathFrameNumber = deathFrameNumber
-        this.isDead = () => { return this.hp <= 0}
       }
 
 }
+
