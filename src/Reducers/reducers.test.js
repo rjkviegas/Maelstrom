419e0b04ecf134ed91f9e16c35ba8d80041e4210
Harry LB <harry.lingardbright@gmail.com>
2020-09-23 13:48:13 +0100

    fully tested player reducer to 100% coverage

--- src/Reducers/reducers.test.js
+++ src/Reducers/reducers.test.js
@@ -7,11 +7,40 @@ describe('player reducer', () => {
     expect(playerReducer(undefined, mockCallBack)).toBeTruthy();
     expect(playerReducer(undefined, {})).toEqual({});
   });
-  // it('attacked reduces health', () => {
-  //   const [PlayerObj, dispatch] = useReducer(playerReducer, new player())
-  //   dispatch({type: 'attacked', payload: 10})
-  //   playerReducer(PlayerObj, { type: "attacked", payload: 10 })
- 
-  //   expect((PlayerObj.hp)).toEqual(90);
-  // });
+  
+  it('should handle attacked', () => {
+    expect(playerReducer({hp: 1000}, {
+        type: "attacked",
+        payload: 10
+      })).toEqual({
+        hp: 990
+      })
+  });
+
+  it('should handle set_attack', () => {
+    expect(playerReducer({is_attacking: false}, {
+      type: "set_attack",
+      payload: true
+    })).toEqual({
+      is_attacking: true
+    })
+  });
+
+  it('should handle unset_attack', () => {
+    expect(playerReducer({is_attacking: true}, {
+      type: "unset_attack",
+      payload: false
+    })).toEqual({
+      is_attacking: false
+    })
+  });
+  
+  it('should handle reset', () => {
+    expect(playerReducer({hp: 2, MAX_HP: 100}, {
+      type: "reset",
+    })).toEqual({
+      hp: 100,
+      MAX_HP:100
+    })
+  });
 });
