0a7e4316cbaaf938150984ff4850af12cf68c6f2
DillonBarker <barker.dill98@gmail.com>
2020-09-23 12:07:44 +0100

    adding to the shop displays two items, need to add logic to methods

--- src/Components/canvas/shopCanvas.js
+++ src/Components/canvas/shopCanvas.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import PlayerContext from '../../config/playerContext.js';
 import Gold from '../gold/gold.js'
 
@@ -10,10 +9,16 @@ const ShopCanvas = () => {
 
             <Gold PlayerObj={PlayerObj}/>
 
-            <button id="item">Sword</button>
-            <button id="item">Shield</button>
-            <button id="item">Health Potion</button>
- 
+                <div class="item1">
+                    <button id="item" onClick={"minusGold(); addAttack()"}>Sword</button>
+                </div>
+                <p class="hide1">The Sword of 1000 truths, once said to belong to King Arthur... But that myth is lost to the Maelstrom. Grants extra damage.</p>
+
+                <div class="item1">
+                    <button id="item" onClick={"minusGold(); addDefence()"} >Shield</button>
+                </div>
+                <p class="hide1">A shield, once wielded by a great Ragnar Lothbrok who fought in the shield wall attacking East Anglia. Grants extra defence.</p>
+                
         </div>
     )
 }
