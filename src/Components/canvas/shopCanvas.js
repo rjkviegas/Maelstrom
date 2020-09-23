ae86c2cd180281f9c1fbd0165a3d0fe81ab33763
DillonBarker <barker.dill98@gmail.com>
2020-09-23 12:31:13 +0100

    adding class names

--- src/Components/canvas/shopCanvas.js
+++ src/Components/canvas/shopCanvas.js
@@ -9,15 +9,15 @@ const ShopCanvas = () => {
 
             <Gold PlayerObj={PlayerObj}/>
 
-                <div class="item1">
+                <div className="item1">
                     <button id="item" onClick={"minusGold(); addAttack()"}>Sword</button>
                 </div>
-                <p class="hide1">The Sword of 1000 truths, once said to belong to King Arthur... But that myth is lost to the Maelstrom. Grants extra damage.</p>
+                <p className="hide1">The Sword of 1000 truths, once said to belong to King Arthur... But that myth is lost to the Maelstrom. Grants extra damage.</p>
 
-                <div class="item1">
+                <div className="item1">
                     <button id="item" onClick={"minusGold(); addDefence()"} >Shield</button>
                 </div>
-                <p class="hide1">A shield, once wielded by a great Ragnar Lothbrok who fought in the shield wall attacking East Anglia. Grants extra defence.</p>
+                <p className="hide1">A shield, once wielded by a great Ragnar Lothbrok who fought in the shield wall attacking East Anglia. Grants extra defence.</p>
                 
         </div>
     )
