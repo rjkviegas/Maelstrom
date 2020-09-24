e683f0bbd9bf3b24d23e146295f7d695b1017759
Daniel M <danielim@hotmail.co.uk>
2020-09-24 13:22:54 +0100

    Objectified shop items to be responsive. Ready for single reducer

--- src/Components/canvas/shopCanvas.js
+++ src/Components/canvas/shopCanvas.js
@@ -6,16 +6,25 @@ import PlayerHealthBar from '../healthbar/healthbar.js'
 
 const ShopCanvas = () => {
     const SWORD_STRENGTH = 5;
+    const SWORD_COST = 200;
     const SHIELD_DEFENCE = 5;
-    const HEALTH_POTION = 10;
+    const SHIELD_COST = 200;
+    const HEALTH_POTION_HP_BOOST = 10;
+    const HEALTH_POTION_COST = 10;
     const MIN_MONIES = 200;
-    const POT_COST = 40;
+
 
     const { PlayerObj, dispatch }  = useContext(PlayerContext)
     
     let history = useHistory()
     function handleClick() {
         history.push('/play');
+    }
+
+    const items = {
+        sword: {strength: SWORD_STRENGTH, defence: 0, cost: SWORD_COST, hp_boost: 0},
+        shield: {strength: 0, defence: SHIELD_DEFENCE, cost: SHIELD_COST, hp_boost: 0},
+        health_potion: {strength: 0, defence: 0, cost: HEALTH_POTION_COST, hp_boost: HEALTH_POTION_HP_BOOST} 
     }
 
     const buySword = () => {
@@ -35,21 +44,21 @@ const ShopCanvas = () => {
     }
     
     const buyHealthPot = () => {
-        if (PlayerObj.money < POT_COST ) return;
+        if (PlayerObj.money < HEALTH_POTION_COST ) return;
         if (PlayerObj.hp < PlayerObj.MAX_HP) return;
 
-        dispatch({type: 'TAKEN_HEALTH_POTION', payload: HEALTH_POTION })
-        dispatch({type: 'DEDUCTED_MONEY', payload: POT_COST});
+        dispatch({type: 'TAKEN_HEALTH_POTION', payload: HEALTH_POTION_HP_BOOST })
+        dispatch({type: 'DEDUCTED_MONEY', payload: HEALTH_POTION_COST});
     }
     
-    let visibleSword = (<div><div className="item1"><button data-testid="sword-button" id="item" onClick={buySword}>Sword 200ɓ</button></div>
-        <p className="hide1">The Sword of 1000 truths, once said to belong to King Arthur... But that myth is lost to the Maelstrom. Grants extra damage. Costs 200 blasei shards.</p></div>)
+    let visibleSword = (<div><div className="item1"><button data-testid="sword-button" id="item" onClick={buySword}>Sword {items.sword.cost}ɓ</button></div>
+        <p className="hide1">The Sword of 1000 truths, once said to belong to King Arthur... But that myth is lost to the Maelstrom. Grants extra {items.sword.strength} damage stats. Costs {items.sword.cost} blasei shards.</p></div>)
 
-    let visibleShield = (<div><div className="item1"><button data-testid="shield-button" id="item" onClick={buyShield}>Shield 200ɓ</button></div>
-        <p className="hide1">A shield, once wielded by a great Ragnar Lothbrok who fought in the shield wall attacking East Anglia. Grants extra defence. Costs 200 blasei shards.</p></div>)
+    let visibleShield = (<div><div className="item1"><button data-testid="shield-button" id="item" onClick={buyShield}>Shield {items.shield.cost}ɓ</button></div>
+        <p className="hide1">A shield, once wielded by a great Ragnar Lothbrok who fought in the shield wall attacking East Anglia. Grants extra {items.shield.defence} defence stats. Costs {items.shield.cost} blasei shards.</p></div>)
 
-    let healthPot = (<div><div className="item1"><button data-testid="healthpot-button" id="item" onClick={buyHealthPot}>Health Potion 40ɓ</button></div>
-        <p className="hide1">A natural concoction of sorts, brewed and distilled by the Su'lgaryan Druids who inhabit the dense forestland. Grants 10 HP. Costs 40 blasei shards.</p></div>)
+    let healthPot = (<div><div className="item1"><button data-testid="healthpot-button" id="item" onClick={buyHealthPot}>Health Potion {items.health_potion.cost}ɓ</button></div>
+        <p className="hide1">A natural concoction of sorts, brewed and distilled by the Su'lgaryan Druids who inhabit the dense forestland. Grants {items.health_potion.hp_boost} HP. Costs {items.health_potion.cost} blasei shards.</p></div>)
     
     return (
         <div data-testid="shop" style={{height: '400px'}}>
