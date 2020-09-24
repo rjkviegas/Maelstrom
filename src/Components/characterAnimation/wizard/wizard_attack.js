676c4ea4b3cfa32cbf27142d9fd804cae870fb2d
Harry LB <harry.lingardbright@gmail.com>
2020-09-24 17:02:19 +0100

    fixed BOY

--- src/Components/characterAnimation/wizard/wizard_attack.js
+++ src/Components/characterAnimation/wizard/wizard_attack.js
@@ -1,7 +1,7 @@
 export const wizardAttack = new Image();
 wizardAttack.src = "./assets/characterSprites/Evil_Wizard_2/Attack1.png";
-wizardAttack.width = wizardAttack.width/8
-wizardAttack.height = wizardAttack.height/1
+wizardAttack.width = 2000/8
+// wizardAttack.height = wizardAttack.height/1
 wizardAttack.yOffset = -25
 wizardAttack.xOffset = -40
 wizardAttack.cycleLoop = [0, 1, 2, 3, 4, 5, 6, 7];
